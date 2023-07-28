4648b9518d757b3733b3f659e019c1558731007e
J0nathan550 <ihudokormov@gmail.com>
2023-07-28 13:51:53 +0300

    Changed app icons.

--- Pages/AuthorPage.xaml.cs
+++ Pages/AuthorPage.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
 namespace Weather_App.Pages;
 
 public partial class AuthorPage : ContentPage
@@ -9,11 +12,50 @@ public partial class AuthorPage : ContentPage
 
     private void TelegramButtonClicked(object sender, EventArgs e)
     {
-
+        OpenBrowser("https://t.me/brenntkopfsd");
     }
 
     private void YouTubeButtonClicked(object sender, EventArgs e)
     {
+        OpenBrowser("https://www.youtube.com/channel/UCZhjs2f-DsO7SXf5FJCKbxA");
+    }
 
+    private void PlayMarketButtonClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WebAPIButtonClicked(object sender, EventArgs e)
+    {
+        OpenBrowser("https://www.weatherapi.com");
+    }
+
+    private async void OpenBrowser(string link)
+    {
+        try
+        {
+            Uri uri = new Uri(link);
+            BrowserLaunchOptions options = new BrowserLaunchOptions()
+            {
+                LaunchMode = BrowserLaunchMode.SystemPreferred,
+                TitleMode = BrowserTitleMode.Show,
+                PreferredToolbarColor = Color.Parse("#fc7f03"),
+                PreferredControlColor = Color.Parse("#fc7f03")
+            };
+
+            await Browser.Default.OpenAsync(uri, options);
+        }
+        catch (Exception ex)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            string text = ex.Message;
+            ToastDuration duration = ToastDuration.Short;
+            double fontSize = 14;
+
+            var toast = Toast.Make(text, duration, fontSize);
+
+            await toast.Show(cancellationTokenSource.Token);
+        }
     }
 }
